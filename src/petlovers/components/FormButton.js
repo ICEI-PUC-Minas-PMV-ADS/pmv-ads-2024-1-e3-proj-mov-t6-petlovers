69806a68ae1f903a7e9292218d5ba5ce406586e9
Alissa <alissanishihara@gmail.com>
2024-04-02 18:13:02 +0200

    increase buttom

--- src/petlovers/components/FormButton.js
+++ src/petlovers/components/FormButton.js
@@ -11,7 +11,7 @@ const MyComponent = (props) => (
 const styles = StyleSheet.create({
   button: {
     backgroundColor: '#827397',
-    width: 300,
+    width: 335,
     marginTop: 10,
     padding: 5,
     textTransform: 'none',
