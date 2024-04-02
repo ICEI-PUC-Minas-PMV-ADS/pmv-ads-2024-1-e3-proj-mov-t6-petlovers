f2cb5ee68c58e3a32ea39da46c944929889469cc
Alissa <alissanishihara@gmail.com>
2024-04-02 17:19:07 +0200

    fix title margin

--- src/petlovers/components/Header.js
+++ src/petlovers/components/Header.js
@@ -3,10 +3,11 @@ import { Appbar } from 'react-native-paper';
 
 const Header = () => {
   return (
-    <Appbar.Header style={styles.header}>
+  <Appbar.Header style={styles.header}>
       <Appbar.Content
         title="PetLovers"
         titleStyle={styles.title}
+        style={styles.content}
       />
     </Appbar.Header>
   );
@@ -19,6 +20,12 @@ const styles = {
   title: {
     fontWeight: 'bold',
     color: '#5F5B5B',
+    marginLeft: -20, 
+  },
+  content: {
+    alignItems: 'flex-start', // Ajusta o alinhamento do conteúdo para a esquerda
+    justifyContent: 'center', // Centraliza o conteúdo verticalmente
+    flex: 1, // Garante que o conteúdo ocupe todo o espaço disponível
   },
 };
 
