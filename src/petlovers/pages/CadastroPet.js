58305b6b45e34efb88b430e76fafb5ddbcae9bc6
Alissa <alissanishihara@gmail.com>
2024-04-18 22:16:52 +0200

    add image picker

--- src/petlovers/pages/CadastroPet.js
+++ src/petlovers/pages/CadastroPet.js
@@ -1,21 +1,33 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { TextInput } from 'react-native-paper';
-import { SafeAreaView, StyleSheet, View, Text, ScrollView, Alert } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, ScrollView, Alert, Image , TouchableOpacity} from 'react-native';
+import {  } from "expo-status-bar";
+import * as ImagePicker from 'expo-image-picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 import ArrowLeft from '../components/ArrowLeft';
 import FormButton from '../components/FormButton';
 import FormInput from '../components/FormInput';
 
+
 const CadastroPet = () => {
-  const [nome, setNome] = React.useState("");
-  const [idade, setIdade] = React.useState("");
-  const [cidade, setCidade] = React.useState("");
-  const [sexo, setSexo] = React.useState("");
-  const [cor, setCor] = React.useState("");
-  const [raca, setRaca] = React.useState("");
-  const [porte, setPorte] = React.useState("");
-  const [sobre, setSobre] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [age, setAge] = React.useState("");
+  const [city, setCity] = React.useState("");
+  const [state, setState] = React.useState("");
+  const [sex, setSex] = React.useState("");
+  const [color, setColor] = React.useState("");
+  const [breed, setBreed] = React.useState("");
+  const [size, setSize] = React.useState("");
+  const [about, setAbout] = React.useState("");
 
   const handleCadastro = async () => {
+    if (!name ||!age ||!city ||!state ||!sex ||!color ||!breed ||!size ||!about) {
+      alert("Todos os campos são obrigatórios");
+      return;
+    }
+
     try {
       const response = await fetch("http://localhost:3000/api/pet", {
         method: 'POST',
@@ -23,14 +35,15 @@ const CadastroPet = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          nome,
-          idade,
-          cidade,
-          sexo,
-          cor,
-          raca,
-          porte,
-          sobre,
+          name,
+          age,
+          city,
+          state,
+          sex,
+          color,
+          breed,
+          size,
+          about,
         }),
       });
 
@@ -40,37 +53,64 @@ const CadastroPet = () => {
 
       Alert.alert('Sucesso', 'Pet cadastrado com sucesso!');
       // Limpar os campos após o cadastro
-      setNome("");
-      setIdade("");
-      setCidade("");
-      setSexo("");
-      setCor("");
-      setRaca("");
-      setPorte("");
-      setSobre("");
+      setName("");
+      setAge("");
+      setCity("");
+      setState("");
+      setSex("");
+      setColor("");
+      setBreed("");
+      setSize("");
     } catch (error) {
       console.error('Erro ao cadastrar pet:', error);
-      Alert.alert('Erro', 'Falha ao cadastrar pet. Por favor, tente novamente mais tarde.');
+      Alert.alert('Erro', 'Falha ao cadastrar pet. Por favor, tente novamente.');
     }
   };
 
+  //IMAGE PICKER
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+  
+    console.log(result);
+  
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      setImage(result.assets[0].uri);
+    }
+  };
+  
+
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView>
       <ArrowLeft />
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
         <View style={styles.textAreaContainer}>
           <Text style={styles.title}>Cadastre o seu pet</Text>
-          <FormInput label='Nome pet' value={nome} onChangeText={setNome} style={styles.input} />
-          <FormInput label='Idade' value={idade} onChangeText={setIdade} keyboardType="number-pad" style={styles.input} />
-          <FormInput label='Cidade' value={cidade} onChangeText={setCidade} style={styles.input} placeholderTextColor="grey" />
-          <FormInput label='Sexo' value={sexo} onChangeText={setSexo} style={styles.input} placeholderTextColor="grey" />
-          <FormInput label='Cor' value={cor} onChangeText={setCor} style={styles.input} />
-          <FormInput label='Raça' value={raca} onChangeText={setRaca} style={styles.input} />
-          <FormInput label='Porte' value={porte} onChangeText={setPorte} style={styles.input} />
+          <View style={styles.container}>
+           <TouchableOpacity onPress={pickImage}>
+           <Icon name="camera" size={30} color="#827397" />
+           </TouchableOpacity>
+           {image && <Image source={{ uri: image }} style={styles.image} />}
+         </View>
+          <FormInput label='Nome pet' value={name} onChangeText={setName} style={styles.input} />
+          <FormInput label='Idade' value={age} onChangeText={setAge} keyboardType="number-pad" style={styles.input} />
+          <FormInput label='Cidade' value={city} onChangeText={setCity} style={styles.input} placeholderTextColor="grey" />
+          <FormInput label='Estado' value={state} onChangeText={setState} style={styles.input} placeholderTextColor="grey" />
+          <FormInput label='Sexo' value={sex} onChangeText={setSex} style={styles.input} placeholderTextColor="grey" />
+          <FormInput label='Cor' value={color} onChangeText={setColor} style={styles.input} />
+          <FormInput label='Raça' value={breed} onChangeText={setBreed} style={styles.input} />
+          <FormInput label='Porte' value={size} onChangeText={setSize} style={styles.input} />
           <TextInput
             style={styles.textArea}
-            value={sobre}
-            onChangeText={setSobre}
+            value={about}
+            onChangeText={setAbout}
             placeholder="Sobre o seu pet..."
             placeholderTextColor="grey"
             numberOfLines={10}
@@ -80,13 +120,20 @@ const CadastroPet = () => {
           <FormButton style={styles.cadastrarButton} onPress={handleCadastro}>Cadastrar</FormButton>
         </View>
       </ScrollView>
+
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollViewContent: {
-    flexGrow: 1,
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
   textAreaContainer: {
     padding: 20,
@@ -95,10 +142,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: 20,
+    marginBottom: 10,
   },
   textArea: {
-    height: 250,
+    height: 150,
     justifyContent: "flex-start",
     marginTop: 20,
   },
@@ -106,6 +153,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#827397',
     width: 352,
     marginTop: 20,
+    marginBottom: 60,
     padding: 5,
     textTransform: 'none',
     borderRadius: 10,
---
+++ src/petlovers/services/HandleImageService.js
@@ -0,0 +1,23 @@
+import * as ImagePicker from 'expo-image-picker';
+
+export const pickImage = async () => {
+  try {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      return result.assets[0].uri;
+    }
+  } catch (error) {
+    console.error('Erro ao selecionar imagem:', error);
+    throw new Error('Falha ao selecionar imagem. Por favor, tente novamente.');
+  }
+};
+
+
