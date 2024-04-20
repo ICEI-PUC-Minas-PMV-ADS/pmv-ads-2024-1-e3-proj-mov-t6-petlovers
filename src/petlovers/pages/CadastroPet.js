2440cfb7413ffb0d606f13cad26e1060badf85f7
Alissa <alissanishihara@gmail.com>
2024-04-20 20:02:48 +0200

    add image picker

--- src/petlovers/pages/CadastroPet.js
+++ src/petlovers/pages/CadastroPet.js
@@ -1,14 +1,20 @@
-import * as React from "react";
+import React, { useState } from 'react';
 import { TextInput } from "react-native-paper";
 import {
   SafeAreaView,
   StyleSheet,
   View,
   Text,
-  ScrollView,
   Alert,
+  TouchableOpacity,
+  Image,
 } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import {  } from "expo-status-bar";
+import * as ImagePicker from 'expo-image-picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
 
 import ArrowLeft from "../components/ArrowLeft";
 import FormButton from "../components/FormButton";
@@ -71,12 +77,38 @@ const CadastroPet = () => {
     }
   };
 
+  //IMAGE PICKER
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ArrowLeft />
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+      <KeyboardAwareScrollView >
         <View style={styles.textAreaContainer}>
           <Text style={styles.title}>Cadastre o seu pet</Text>
+          <View style={styles.container}>
+              <View style={styles.imageContainer}>
+                <TouchableOpacity onPress={pickImage} style={styles.cameraButton}>
+                  <Icon name="camera" size={40} color="#827397" />
+                </TouchableOpacity>
+               {image && <Image source={{ uri: image }} style={styles.image} />}
+              </View>
+            </View>
           <FormInput
             label="Nome pet"
             value={nome}
@@ -136,7 +168,7 @@ const CadastroPet = () => {
             Cadastrar
           </FormButton>
         </View>
-      </ScrollView>
+      </KeyboardAwareScrollView>
     </SafeAreaView>
   );
 };
@@ -144,6 +176,33 @@ const CadastroPet = () => {
 const styles = StyleSheet.create({
   scrollViewContent: {
     flexGrow: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 200,
+    height: 200,
+    borderRadius: 100, 
+    backgroundColor:'white',
+    overflow: 'hidden', 
+  },
+  cameraButton: {
+    position: 'absolute',
+    zIndex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    color:"#FFFFFF",
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
   textAreaContainer: {
     padding: 20,
@@ -153,23 +212,36 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     marginBottom: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
   },
   textArea: {
     height: 100,
+    height: 120,
     justifyContent: "flex-start",
     marginTop: 20,
   },
   cadastrarButton: {
     backgroundColor: "#827397",
+    backgroundColor: '#827397',
     width: 352,
     marginTop: 20,
+    marginBottom: 50,
     padding: 5,
     textTransform: "none",
+    textTransform: 'none',
     borderRadius: 10,
   },
   input: {
     marginTop: 10,
     color: "grey",
+    color: "grey"
+  },
+  errorMessage: {
+    color: 'red',
+    fontSize: 14,
+    marginTop: 5,
   },
 });
 
