c6e978e92272b5b20dbbf836acad2419cf4c4a03
Alissa <alissanishihara@gmail.com>
2024-05-07 17:38:53 +0200

    change url

--- src/petlovers/apiConfig.js
+++ src/petlovers/apiConfig.js
@@ -1,4 +1,4 @@
-export const baseAPI_URL = 'http//localhost:3000'; // Para testar no celular, substituir 'localhost' pelo endereço IP:3000 (porta do servidor)
+export const baseAPI_URL = 'http://localhost:3000'; // Para testar no celular, substituir 'localhost' pelo endereço IP:3000 (porta do servidor)
 
 export const userAPI_URL = `${baseAPI_URL}/api/user`;
 export const getAllPetsAPI_URL = `${baseAPI_URL}/api/allpets`;
