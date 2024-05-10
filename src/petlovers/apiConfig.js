b4fa26c9afa4db6a7cb1e747eda41b10e9497207
Alissa <alissanishihara@gmail.com>
2024-05-10 18:10:48 +0200

    add pet url

--- src/petlovers/apiConfig.js
+++ src/petlovers/apiConfig.js
@@ -5,3 +5,4 @@ export const getAllPetsAPI_URL = `${baseAPI_URL}/api/allpets`;
 //export const petimageAPI_URL = `${baseAPI_URL}/api/pet/${petId}/images`;
 export const cardpetsAPI_URL = `${baseAPI_URL}/api/fourpets`;
 export const AvaliacaoAPI_URL = `${baseAPI_URL}/api/avaliacao`;
+export const petAPI_URL = `${baseAPI_URL}/api/pet`;
