c6c7a832b22f0e45433f1c55d3434d7bfa6f5a69
anabmilk <anabmilk@gmail.com>
2024-05-10 18:06:30 -0300

    update SDK 51

--- src/petlovers/apiConfig.js
+++ src/petlovers/apiConfig.js
@@ -1,4 +1,4 @@
-export const baseAPI_URL = 'http://localhost:3000'; // Para testar no celular, substituir 'localhost' pelo endereço IP:3000 (porta do servidor)
+export const baseAPI_URL = 'http://192.168.0.110:3000'; // Para testar no celular, substituir 'localhost' pelo endereço IP:3000 (porta do servidor)
 
 export const userAPI_URL = `${baseAPI_URL}/api/user`;
 export const getUserByIdAPI_URL = (userId) => `${baseAPI_URL}/api/user/${userId}`; // Rota para obter um usuário pelo ID
