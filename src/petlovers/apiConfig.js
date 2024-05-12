cf407a8050d7b033e0ad229cee6d993d310f1e20
Alissa <alissanishihara@gmail.com>
2024-05-12 20:13:39 +0200

    implement function to get pet data associated with user

--- src/petlovers/apiConfig.js
+++ src/petlovers/apiConfig.js
@@ -1,4 +1,4 @@
-export const baseAPI_URL = 'http://192.168.0.110:3000'; // Para testar no celular, substituir 'localhost' pelo endereço IP:3000 (porta do servidor)
+export const baseAPI_URL = 'http://192.168.1.239:3000'; // Para testar no celular, substituir 'localhost' pelo endereço IP:3000 (porta do servidor)
 
 export const userAPI_URL = `${baseAPI_URL}/api/user`;
 export const getUserByIdAPI_URL = (userId) => `${baseAPI_URL}/api/user/${userId}`; // Rota para obter um usuário pelo ID
@@ -7,3 +7,5 @@ export const getAllPetsAPI_URL = `${baseAPI_URL}/api/allpets`;
 export const cardpetsAPI_URL = `${baseAPI_URL}/api/fourpets`;
 export const AvaliacaoAPI_URL = `${baseAPI_URL}/api/avaliacao`;
 export const petAPI_URL = `${baseAPI_URL}/api/pet`;
+
+
--- src/petlovers/pages/DadosPet.js
+++ src/petlovers/pages/DadosPet.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextInput } from "react-native-paper";
 import { SafeAreaView, StyleSheet, View, Text, Alert, TouchableOpacity, Image, } from "react-native";
-import { useRoute } from "@react-navigation/native";
-import Icon from 'react-native-vector-icons/FontAwesome';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { getAuth } from "firebase/auth";
 import ArrowLeft from "../components/ArrowLeft";
@@ -10,77 +8,105 @@ import FormButton from "../components/FormButton";
 import FormInput from "../components/FormInput";
 import validateCadastroPet from '../services/validateCadastroPet';
 
-//API URL do cadastro pet
-import { petAPI_URL } from "../apiConfig";
+import {baseAPI_URL} from '../apiConfig';
 
 const DadosPet = ({ }) => {
-  
- return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ArrowLeft />
-      <KeyboardAwareScrollView >
-        <View style={styles.textAreaContainer}>
-          <Text style={styles.title}>Editar dados pet</Text>
-          <View style={styles.container}>
-            <View style={styles.imageContainer}>
-              <TouchableOpacity style={styles.cameraButton}>
-                <Icon name="camera" size={40} color="#827397" />
-              </TouchableOpacity>
-              
-            </View>
-          </View>
-          <FormInput
-            label="Nome pet"
-            style={styles.input}
-          />
-          <FormInput
-            label="Idade"
-            keyboardType="number-pad"
-            style={styles.input}
-          />
-          <FormInput
-            label="Cidade"
-            style={styles.input}
-            placeholderTextColor="grey"
-          />
-          <FormInput
-            label="Estado"
-            style={styles.input}
-            placeholderTextColor="grey"
-          />
-          <FormInput
-            label="Sexo"
-            style={styles.input}
-            placeholderTextColor="grey"
-          />
-          <FormInput
-            label="Cor"
-            style={styles.input}
-          />
-          <FormInput
-            label="Raça"
-            style={styles.input}
-          />
-          <Text style={styles.errorMessage}></Text>
-          <FormInput
-            label="Porte"
-            style={styles.input}
-          />
-          <TextInput
-            style={styles.textArea}
-            placeholder="Sobre o seu pet..."
-            placeholderTextColor="grey"
-            //numberOfLines={10}
-            multiline={false}
-            mode="outlined"
-          />
-          <FormButton style={styles.cadastrarButton}>
-            Cadastrar
-          </FormButton>
-        </View>
-      </KeyboardAwareScrollView>
-    </SafeAreaView>
-  );
+    const [petData, setPetData] = useState(null);
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    useEffect(() => {
+        const fetchPetData = async () => {
+            try {
+                if (!user) return;
+                const userId = user.uid;
+
+                const response = await fetch(`${baseAPI_URL}/api/get-pet-data/${userId}`);
+                const petDataFromServer = await response.json();
+
+
+                setPetData(petDataFromServer.data);
+            } catch (error) {
+                console.error("Erro ao obter dados do pet:", error);
+            }
+        };
+
+        fetchPetData();
+    }, [user]);
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <ArrowLeft />
+            <KeyboardAwareScrollView >
+                <View style={styles.textAreaContainer}>
+                    <Text style={styles.title}>Editar dados pet</Text>
+
+                    {user ? (
+                        <>
+                            <FormInput
+                                label="Nome pet"
+                                style={styles.input}
+                                value={petData?.nome || ''}
+                            />
+                            <FormInput
+                                label="Idade"
+                                keyboardType="number-pad"
+                                style={styles.input}
+                                value={petData?.idade.toString() || ''}
+                            />
+                            <FormInput
+                                label="Cidade"
+                                style={styles.input}
+                                placeholderTextColor="grey"
+                                value={petData?.cidade || ''}
+                            />
+                            <FormInput
+                                label="Estado"
+                                style={styles.input}
+                                placeholderTextColor="grey"
+                                value={petData?.estado || ''}
+                            />
+                            <FormInput
+                                label="Sexo"
+                                style={styles.input}
+                                placeholderTextColor="grey"
+                                value={petData?.sexo || ''}
+                            />
+                            <FormInput
+                                label="Cor"
+                                style={styles.input}
+                                value={petData?.cor || ''}
+                            />
+                            <FormInput
+                                label="Raça"
+                                style={styles.input}
+                                value={petData?.raca || ''}
+                            />
+                            <FormInput
+                                label="Porte"
+                                style={styles.input}
+                                value={petData?.porte || ''}
+                            />
+                            <TextInput
+                                style={styles.textArea}
+                                placeholder="Sobre o seu pet..."
+                                placeholderTextColor="grey"
+                                //numberOfLines={10}
+                                multiline={false}
+                                mode="outlined"
+                                value={petData?.sobre || ''}
+                            />
+                            <FormButton style={styles.cadastrarButton}>
+                                Salvar
+                            </FormButton>
+                        </>
+                    ) : (
+                        <Text style={styles.notlogged}>Por favor, faça login para editar os dados do seu pet.</Text>
+                    )}
+                </View>
+            </KeyboardAwareScrollView>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
@@ -92,30 +118,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  imageContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    backgroundColor: 'white',
-    overflow: 'hidden',
-    marginBottom: 30,
-    marginTop: 30,
-  },
-  cameraButton: {
-    position: 'absolute',
-    zIndex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
-    color: "#FFFFFF",
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-  },
   textAreaContainer: {
     padding: 20,
   },
@@ -126,7 +128,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: 10,
+    marginBottom: 30,
   },
   textArea: {
     height: 100,
@@ -139,6 +141,7 @@ const styles = StyleSheet.create({
     width: 352,
     marginTop: 10,
     marginBottom: 50,
+    marginTop: 20,
     padding: 5,
     textTransform: "none",
     textTransform: 'none',
@@ -152,6 +155,13 @@ const styles = StyleSheet.create({
     color: 'red',
     fontSize: 12,
   },
+  notlogged: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: "#827397",
+    marginTop: 100,
+  }
+  
 });
 
 export default DadosPet;
